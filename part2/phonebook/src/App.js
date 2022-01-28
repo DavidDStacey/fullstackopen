3ff3da433c6225608bc878dfd4d8ce49eb8fb855
DavidDStacey <daviddstacey1999@gmail.com>
2022-01-28 14:00:11 -0600

    changed name to phonebook
    gets data from db.json

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,17 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Search from './components/Search'
 import Form from './components/Form'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  // use db.json instead
+  //const [persons, setPersons] = useState([
+  //  { name: 'Arto Hellas', number: '040-123456', id: 1 },
+  //  { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+  //  { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+  //  { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  //])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
