0ed6ad7a36523e3221ed4a3c3be99bcff4bd61bb
DavidDStacey <daviddstacey1999@gmail.com>
2022-01-28 13:13:18 -0600

    exampleapp updated

--- part2/exampleapp/src/App.js
+++ part2/exampleapp/src/App.js
@@ -1,10 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = ( props ) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  
+  const hook = () => {
+    console.log('effect')
+    axios 
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
 
   // result = condition ? val1 : val2
   // result = val1 if conition is true
