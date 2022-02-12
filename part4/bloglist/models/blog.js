891955966e0720fd29a9b4b6c18d774a965271f7
DavidDStacey <daviddstacey1999@gmail.com>
2022-02-11 19:07:34 -0600

    commit

--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const blogSchema = mongoose.Schema({
   title: { type: String, required: true },
-  author: String,
+  author: { type: String, required: true },
   url: { type: String, required: true },
   likes: { type: Number, default: 0 },
   user: {
